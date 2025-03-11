1d914726014a8ca1c22ae51f1fa4ea059802176f
KauaOSantos <kauaoliveira1506gaf@gmail.com>
2025-03-10 22:58:45 -0400

    header criado e responsividade iniciada

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');   
-
-});
+class NavbarMobile {
+    constructor (menuMobile, navList, navLinks){
+        this menuMobile = document.querySelector(menuMobile);
+        this navList = document.querySelector(navList);
+        this navLinks = document.querySelectorAll(navLinks);
+        this activeClass = "active";
+    }
+    addClickEvent() {
+        this.menuMobile.addEventListner("click", () => console.log ("Hei"));
+    }
+    init() {
+        if(this.menuMobile){
+            this.addClickEvent();
+        }
+        return this
+    }
+}
